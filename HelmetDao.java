37f6ccf46dd671ec07f7a8203aa9e4955627bd77
RehanaMulla33 <89864443+RehanaMulla33@users.noreply.github.com>
2021-12-07 22:28:31 +0530

    Add files via upload

---
+++ HelmetDao.java
@@ -0,0 +1,10 @@
+package com.xworkz.Assignment3.Exception.Helmet.dao;
+
+import com.xworkz.Assignment3.Exception.Helmet.dto.HelmetDto;
+
+public interface HelmetDao {
+	public void save(HelmetDto dto);
+	public void display();
+	
+
+}
